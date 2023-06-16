633dee7b9057b853b856952ee9ac1c56da40995c
nidhijagga <nidhijagga5239@gmail.com>
2023-06-16 19:44:55 +0530

    Finding the Players

--- app.js
+++ app.js
@@ -3,12 +3,58 @@ const express = require("express");   // Importing the Express framework
 const app = express();   // Creating an Express application
 const path = require("path");
 
+const http = require("http");   // Importing the built-in HTTP module
+const {Server} = require("socket.io");   // Importing the Socket.IO library and extracting the Server class
+
+// Creating an HTTP server using the Express application
+const server = http.createServer(app);
+
+// Creating a new instance of Socket.IO by passing the HTTP server
+const io = new Server(server);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", (req, res)=>{
     res.sendFile(path.join(__dirname, 'public', "views", 'index.html'));
 })
 
-app.listen(4000, ()=>{
+let arr = [];
+let playingArr = [];
+
+io.on("connection", (socket)=>{
+    console.log("New user connected");
+
+    //Receiving the name from client.
+    socket.on("find", (e)=>{
+        if(e.name!==null){
+            arr.push(e.name);
+            if(arr.length >= 2){
+                let p1obj = {
+                    p1name : arr[0],
+                    p1value : "Player1",
+                    p1count : 0
+                }
+                let p2obj = {
+                    p2name : arr[1],
+                    p2value : "Player2",
+                    p2count : 0
+                }
+                let obj = {
+                    p1 : p1obj,
+                    p2 : p2obj,
+                }
+                playingArr.push(obj);
+
+                arr.length = 0;
+
+                //Sending the find data of players to client
+                io.emit("find", {allPlayers : playingArr})
+            }
+        }
+    })
+})
+
+
+server.listen(4000, ()=>{
     console.log("Server Connected");
 })
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,51 @@
-document.getElementById("play").innerHTML = "";
+const playElements = document.querySelectorAll(".play");
+playElements.forEach(function (element) {
+  element.style.display = "none";
+});
+document.getElementById("loading").style.display = "none";
 
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
 
+const socket = io();
+
+let name;
+
+document.getElementById("searchBtn").addEventListener("click", function () {
+  name = document.getElementById("name").value;
+  // document.getElementById("user").innerHTML = name;
+
+  if (name === null || name === " ") {
+    alert("Enter a valid name");
+  } else {
+    //Sending the name to server
+    socket.emit("find", { name: name });
+    document.getElementById("loading").style.display = "block";
+    document.getElementById("find").style.display = "none";
+  }
+});
+
+socket.on("find", (e) => {
+  let allPlayersArray = e.allPlayers;
+
+  if (name != "") {
+    document.getElementById("search").remove();
+
+    playElements.forEach(function (element) {
+      element.style.display = "block";
+    });
+
+    diceEl.style.display = "none";
+  }
+
+  let oppName;
+  let value;
+
+  // const foundObject = allPlayersArray.find(obj => obj.p1.p1name == `${name}` || obj.p2.p2name == `${name}`);
+  // foundObject.p1.p1name == `${name}` ? oppName = foundObject.p2.p2name : oppName = foundObject.p1.p1name
+  // foundObject.p1.p1name == `${name}` ? value = foundObject.p1.p1value : value = foundObject.p2.p2value
+
+  // document.getElementById("oppName").innerText = oppName
+  // document.getElementById("value").innerText = value
+});
